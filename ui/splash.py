62541dd02c22d620ea44aee8cabf5e924b268347
Shotgun Ninjas <150827082+shotgunsensei@users.noreply.github.com>
2025-05-13 15:01:40 -0400

    Add files via upload

--- ui/splash.py
+++ ui/splash.py
@@ -1,3 +1,33 @@
+from PyQt5.QtWidgets import QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtCore import Qt, QTimer
 from ui.home import HomeScreen
+import os
 
-class SplashScreen: pass  # Stub
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("TopicFlow - Starting...")
+        self.setFixedSize(500, 300)
+        self.move(400, 200)
+        self.init_ui()
+        QTimer.singleShot(2000, self.show_home)
+
+    def init_ui(self):
+        logo_path = os.path.join("assets", "topicflow_logo.png")
+
+        self.logo = QLabel(self)
+        pixmap = QPixmap(logo_path)
+        self.logo.setPixmap(pixmap.scaled(120, 120, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        self.logo.setAlignment(Qt.AlignCenter)
+        self.logo.setGeometry(190, 40, 120, 120)
+
+        self.text = QLabel("Welcome to TopicFlow", self)
+        self.text.setAlignment(Qt.AlignCenter)
+        self.text.setFont(QFont("Segoe UI", 14))
+        self.text.setGeometry(100, 180, 300, 40)
+
+    def show_home(self):
+        self.home = HomeScreen()
+        self.home.show()
+        self.close()
